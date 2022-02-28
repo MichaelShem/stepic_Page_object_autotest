f287f9aeb691e810f487d0cb02556d45cd877213
Michael_Shem <Minya2044@yandex.ru>
2022-02-28 18:56:35 +0300

    Added is element present

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,18 @@
+from selenium.common.exceptions import NoSuchElementException
 class BasePage():
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10): #Инициализация браузера
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout) #Явное ожидание, каждое действие будет ожидаться 10 секунд или выполняться моментально
 
-    def open(self):
+    def open(self): #Открытие страницы
         self.browser.get(self.url)
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except (NoSuchElementException):
+            return False
+
+
+
