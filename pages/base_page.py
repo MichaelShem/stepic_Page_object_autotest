1619aa89081370781f833ffb28fc37395d4c99c6
Michael_Shem <Minya2044@yandex.ru>
2022-03-03 14:37:25 +0300

    the method of checking the link and going to the login page has been moved to basepage.py and added a test for this check

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,6 +2,7 @@ from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.common.exceptions import NoAlertPresentException # требуется для получения ответа на задание https://stepik.org/lesson/201964/step/2?unit=176022
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from .locators import BasePageLocators
 import math
 
 
@@ -14,6 +15,14 @@ class BasePage():
 
     def open(self): #Открытие страницы
         self.browser.get(self.url)
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+        #return LoginPage(browser=self.browser, url=self.browser.current_url) #Можно в функции сразу вернуть новую инициализацию страницы, что бы не прописывать ее в тесте в ручную
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
+
 
     def is_element_present(self, how, what): #метод провери наличия елемента на странице
         try:
@@ -37,9 +46,7 @@ class BasePage():
         return True
 
 
-
-
-    # требуется для получения ответа на задание https://stepik.org/lesson/201964/step/2?unit=176022
+    # требуется для прохождения капчи в уроке https://stepik.org/lesson/201964/step/2?unit=176022
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
         x = alert.text.split(" ")[2]
--- test_product_page.py
+++ test_product_page.py
@@ -2,7 +2,7 @@ import time
 
 from .pages.product_page import ProductPage
 import pytest
-
+"""
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -56,6 +56,18 @@ def test_message_disappeared_after_adding_product_to_basket(browser, link):
     page.add_product_to_basket()
     page.solve_quiz_and_get_code()
     page.should_disappeared_success_message()
+"""
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
 
 #Запуст теста
-#pytest -v --tb=line --language=en test_guest_can_add_product_to_basket.py
+#pytest -v --tb=line --language=en test_product_page.py
