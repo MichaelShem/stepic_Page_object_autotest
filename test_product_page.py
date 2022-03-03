a1a1968a70727931cac83bb801a8e894acafd580
Michael_Shem <Minya2044@yandex.ru>
2022-03-03 19:50:54 +0300

    made code review

--- test_product_page.py
+++ test_product_page.py
@@ -6,20 +6,20 @@ import pytest
 
 @pytest.mark.quest_add_basket
 class TestQuestAddToBasketFromProductPage():
-    @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
-                                      "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
-                                      "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
-                                      "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
-                                      "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
-                                      "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
-                                      "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
-                                      pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
-                                      "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
-                                      "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+    @pytest.mark.parametrize('link_end', ["?promo=offer0",
+                                      "?promo=offer1",
+                                      "?promo=offer2",
+                                      "?promo=offer3",
+                                      "?promo=offer4",
+                                      "?promo=offer5",
+                                      "?promo=offer6",
+                                      pytest.param("?promo=offer7", marks=pytest.mark.xfail),
+                                      "?promo=offer8",
+                                      "?promo=offer9"])
 
     @pytest.mark.need_review
-    def test_guest_can_add_product_to_basket(self, browser, link):
-        link = f"{link}"
+    def test_guest_can_add_product_to_basket(self, browser, link_end):
+        link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/{link_end}"
         page = ProductPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
         page.open()                       # открываем страницу
         page.add_product_to_basket()      # Добавляем товар в корзину
@@ -53,18 +53,18 @@ class TestNegativeQuestAddToBasketFromProductPage():
         page.should_disappeared_success_message()
 
 
-    def test_guest_should_see_login_link_on_product_page(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-        page = ProductPage(browser, link)
-        page.open()
-        page.should_be_login_link()
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
 
-    @pytest.mark.need_review
-    def test_guest_can_go_to_login_page_from_product_page(self, browser):
-        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
-        page = ProductPage(browser, link)
-        page.open()
-        page.go_to_login_page()
+@pytest.mark.need_review
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
 
 @pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
